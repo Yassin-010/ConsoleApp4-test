4e2015c6ed879365f669169e0106e5b9e08c870a
Yassin Al Farwan <104172581+Yassin-010@users.noreply.github.com>
2024-06-03 11:09:23 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
 
-            Console.WriteLine("Hello, class");
+            Console.WriteLine("Hello, changed from github");
         }
     }
 }
